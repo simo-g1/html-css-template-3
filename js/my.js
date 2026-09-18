94aa23ce8bddef5a94f673b12940f2b6a8580054
Simo <simo@gmail.com>
2026-09-18 18:45:15 +0300

    change v3

--- js/my.js
+++ js/my.js
@@ -12,7 +12,7 @@ let statsSection = document.querySelector(".stats");
 let started = false; // Function Started ? No
 window.onscroll = function () {
     // الجزء بتاع Skills
-    if (window.scrollY >= section.offsetTop - 50) {
+    if (window.scrollY >= section.offsetTop - 287) {
         progressSpans.forEach((span) => {
             span.style.width = span.dataset.width;
         });
